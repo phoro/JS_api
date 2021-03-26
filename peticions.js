163a5f99172d52eba5cd6055ea8f4acf829fc349
robert.cescit <robert.cescit@gmail.com>
2021-03-26 11:31:53 +0100

    millor encapsulament

--- peticions.js
+++ peticions.js
@@ -14,39 +14,48 @@ var programa = function () {
     const color_text = document.getElementById("id_color_text");
     const titol = document.getElementById("id_titol");
     const sortida_std = document.getElementById("id_sortida_std");
+    const boto_temes = document.getElementById("id_bt_temes");
+    const boto_configura = document.getElementById("id_bt_configura");
+    const boto_limpia = document.getElementById("id_bt_limpia");
+    const boto_info = document.getElementById("id_bt_info");
 
-  
-    //Disposa variables i elements en modo de iniciació
-    function inicialitzacio() {
-        seleccio.hidden = true;
-        colors.hidden = true;
-        titol.hidden = true;
-        //TODO han de llegir el color actual
-        color_central.value = "#3EA4ED";
-        color_esq.value = "#348AC7";
-        color_text.value = "#FFFFFF";
-        //alert("autofuncio executada");
-    };
-
+    //AJAX
     const API = encodeURI("https://servicios.ine.es/wstempus/js/ES/OPERACIONES_DISPONIBLES");
     const peticio = new XMLHttpRequest();
     peticio.open("GET", API, true);
     peticio.send(null);
     peticio.responseType = 'json';
 
-   
+    //Disposa variables i elements en modo de inici
+    function inicialitzacio() {
+        seleccio.hidden = true;
+        colors.hidden = true;
+        titol.hidden = true;
+
+        //TODO han de llegir el color actual
+        color_central.value = "#3EA4ED";
+        color_esq.value = "#348AC7";
+        color_text.value = "#FFFFFF";
+
+        //afegeix listeners
+        boto_temes.addEventListener('click', obteDades);
+        boto_configura.addEventListener('click', triaColors);
+        boto_limpia.addEventListener('click', amagarDesplegables);
+        boto_info.addEventListener('click', mostraInfo);
+        seleccio.onchange = () => { alert('selecció capturada: ' + seleccio.value); };
+        //alert("autofuncio executada");
+    };
+
     function obteDades() {
         let resposta = peticio.response;
         let item, opcio, index = 0;
-        sortida_std.innerHTML="Obtenint dades...";
-        
+        sortida_std.innerHTML = "Obtenint dades...";
 
         //omple la llista desplegable
-        // Comprova si la llista ja és plena abans
-        if (!seleccio.hasChildNodes()) {
+        if (!seleccio.hasChildNodes()) {// Comprova si la llista ja és plena
             //crea el primer element de la llista
             opcio = document.createElement('option');
-            opcio.value = 1;
+            opcio.value = 0;
             opcio.innerHTML = "   SELECCIONE UN TEMA";
             seleccio.appendChild(opcio);
 
@@ -65,12 +74,14 @@ var programa = function () {
         titol.hidden = false;
     };
 
-    function mostraInfo(){
-        sortida_std.innerHTML="Programa JavaScript v0.1"
+    function mostraInfo(e) {
+        let info = "Mòdul encapsulat. AJAX. JavaScript v 0.1";
+        sortida_std.innerHTML = info;
         amagarDesplegables();
         sortida_std.hidden = false;
+        console.log(e);
     }
-    
+
     function amagarDesplegables() {
         seleccio.hidden = true;
         colors.hidden = true;
@@ -78,7 +89,6 @@ var programa = function () {
         sortida_std.hidden = true;
     };
 
-    
     function triaColors() {
         amagarDesplegables();
         titol.innerHTML = "Configuración";
@@ -87,25 +97,13 @@ var programa = function () {
     };
     //funcions accessibles des de fora de l'encapsulament
     return {
-        temes: function () {
-            return peticio.onload = obteDades;//espera a obtenir la resposta abans de cridar
-        },
-        configura: function () {
-            return peticio.onload = triaColors();//espera a obtenir la resposta abans de cridar
-        },
-        inicia: function () {
-            return inicialitzacio();
-        },
-        neteja: function () {
-            return amagarDesplegables();
-        },
-        info: function () {
-            return mostraInfo();
-        }
+        inicia: () => { return inicialitzacio(); },
+        unaltreFuncio: () => { return 0 }
     }
 }
 
 //Funció autoexecutable que es crida a sí mateixa.
+//Inicia el programa
 var init_programa_r0B = (function () {
     programa().inicia();
 })();
